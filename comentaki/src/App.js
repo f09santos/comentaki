b58605460e111e2f8628f83df3d536d7b5c75cb2
Fabio Santos <f09santos@gmail.com>
2022-07-02 21:12:57 -0300

    Adicionando o App

---
+++ comentaki/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css'
+
+import firebase from './firebase';
+
+
+const ref = firebase.database().ref('test')
+ref.on('value', snapshot => {
+  console.log(snapshot.val())
+})
+
+
+
+
+
+function App() {
+  return (
+
+    <div >
+       
+    </div>
+  )
+}
+
+export default App;
